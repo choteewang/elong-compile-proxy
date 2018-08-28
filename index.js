5bbc92dd61da3ebe02640d799bdbce7d52c8b142
王锐 <rui.wang@ly.com>
2018-08-28 15:14:42 +0800

    fixed wrong path inject

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let build = require('./node_modules/ctw-fork-taro-cli/src/weapp').build
 
+console.log('weappJS')
+console.log(weappJs)
+let build = weappJs.build
+console.log('build')
+console.log(build)
+
 module.export = build
